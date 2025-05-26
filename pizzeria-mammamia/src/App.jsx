661a42c708a94c6bd19e44d29d77f9d7b4fc74b4
Sebastian Cabrera <sebastian.cabreracepeda@gmail.com>
2025-05-26 18:04:30 -0400

    Modificacion para mostrar cart

--- pizzeria-mammamia/src/App.jsx
+++ pizzeria-mammamia/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <NavBar />
       {/* <Formulario /> */}
       {/* <Login /> */}
-      <Home />
-      {/* <Cart /> */}
+      {/* <Home /> */}
+      <Cart />
       <Footer />
     </>
   );
